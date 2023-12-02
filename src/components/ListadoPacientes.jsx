c373239bd18d062e63e4cceb8030232ac06e041a
LuisCode1 <64246172+b4dow@users.noreply.github.com>
2023-12-02 23:29:30 +0100

    Creando el proyecto de Seguimiento Veterinaria

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import Paciente from './Paciente'
+
+const ListadoPacientes = ({ pacientes, setPaciente,eliminarPaciente }) => {
+  useEffect(() => {}, [pacientes])
+  return (
+    <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center '>
+            Listado de Pacientes
+          </h2>
+          <p className='text-xl mt-5 text-center mb-10'>
+            Administra tus {''}
+            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+          {pacientes?.map(paciente => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center '>No hay pacientes</h2>
+          <p className='text-xl mt-5 text-center mb-10'>
+            Comienza agregando pacientes y {''}
+            <span className='text-indigo-600 font-bold'>
+              apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ListadoPacientes
